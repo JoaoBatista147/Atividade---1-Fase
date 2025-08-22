f62eafd9ada2bc27a093eff0b3dd630e0467986c
João Batista <joaobatistshh@gmail.com>
2025-08-21 23:50:06 -0300

    Update PrecoInvalidoException.java

--- bomb/src/Produto/PrecoInvalidoException.java
+++ bomb/src/Produto/PrecoInvalidoException.java
@@ -1,5 +1,7 @@
-package Excecoes;
+package Produto;
 
-public class PrecoInvalidoException {
-
+public class PrecoInvalidoException extends Exception {
+    public PrecoInvalidoException(String mensagem) {
+        super(mensagem);
+    }
 }
